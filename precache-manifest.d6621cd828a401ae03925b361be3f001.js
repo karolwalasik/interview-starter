8ff742b8c4a1bda8d12537a4635e902d727df1bf
Karol Walasik <k.walasik@getprintbox.com>
2020-11-15 23:36:42 +0100

    Updates

---
+++ precache-manifest.d6621cd828a401ae03925b361be3f001.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f4bfc2a9cfbd1ef976d40f1a9faf67",
+    "url": "/interview-starter/index.html"
+  },
+  {
+    "revision": "3faec069ff9e0857e847",
+    "url": "/interview-starter/static/css/main.1acb4edd.chunk.css"
+  },
+  {
+    "revision": "c7b28e079bb04dad7be8",
+    "url": "/interview-starter/static/js/2.0873a637.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/interview-starter/static/js/2.0873a637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3faec069ff9e0857e847",
+    "url": "/interview-starter/static/js/main.788d7cda.chunk.js"
+  },
+  {
+    "revision": "1deb427172fbdc05bfcc",
+    "url": "/interview-starter/static/js/runtime-main.76a9f51a.js"
+  },
+  {
+    "revision": "fcf9f4a087c6184f5f9ca20ccf01aa86",
+    "url": "/interview-starter/static/media/Bitmap.fcf9f4a0.png"
+  },
+  {
+    "revision": "490737834295541dd1b855bab6409c5b",
+    "url": "/interview-starter/static/media/checkMark.49073783.svg"
+  },
+  {
+    "revision": "c61579214628da43da6cfe91b681131a",
+    "url": "/interview-starter/static/media/close.c6157921.svg"
+  },
+  {
+    "revision": "46fda88a2fdc968aa54384ac7e35f67f",
+    "url": "/interview-starter/static/media/search.46fda88a.svg"
+  },
+  {
+    "revision": "c6551d465756c3e008b285a8b2b430d5",
+    "url": "/interview-starter/static/media/task-list-plain.c6551d46.svg"
+  }
+]);
